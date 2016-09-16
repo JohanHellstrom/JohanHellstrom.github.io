9abb599672bc4ef99e60e6804a08f5462d3fd3f3
JohanHellstrom <johan.oskar.hellstrom@gmail.com>
2016-09-16 10:09:03 +0200

    Create index.noob.js

---
+++ index.noob.js
@@ -0,0 +1,12 @@
+Chartist.Line('.ct-chart', {
+  labels: ['1600', '1700', '1800', '1850', '1900', '2000'],
+  series: [
+    [15, 14, 10, 2, 1, 1],
+    [1, 1, 2, 6, 2, 15]
+  ]
+}, {
+  fullWidth: true,
+  chartPadding: {
+    right: 40
+  }
+});
